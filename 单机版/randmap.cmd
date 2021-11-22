34d7ed1635da2abadd20f8f393e878e0fe93cfd8
yyhhenry <yyhhenry@foxmail.com>
2021-11-22 16:31:28 +0800

    I don't know what it is

--- 单机版/randmap.cmd
+++ 单机版/randmap.cmd
@@ -2,5 +2,7 @@
 if "%1" == "h" goto begin
 mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
 :begin
+g++ map.cpp -o map.exe 
 map.exe
+del map.exe
 java -jar ghit.jar 
